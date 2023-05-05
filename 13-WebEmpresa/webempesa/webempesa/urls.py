5c6f860473f8a5c15f1ed11440849f0f9aea2f78
joseDG <josethp1@gmail.com>
2023-05-05 17:12:21 -0500

    Web empresa

---
+++ 13-WebEmpresa/webempesa/webempesa/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+
+urlpatterns = [
+    path('', include('core.urls')),
+    path('blog/', include('blog.urls')),
+    path('page/', include('pages.urls')),
+    path('services/', include('services.urls')),
+    path('admin/', admin.site.urls),
+
+    path('contact/', include('contact.urls')),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
